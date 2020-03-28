abd0784c0cf50c3bdd44f21e5499da2c8ae135eb
Norton Li <nli@zipcodes-MBP.home>
2020-03-28 10:38:03 -0400

    second part updated of calculator

--- calculator.py
+++ calculator.py
@@ -269,3 +269,225 @@ def choose_op():
     elif op == 28:
         condi = False
         print('Goodbye!')
+
+
+#### Large Loop Functions ####
+
+# Addition
+
+def add(a,b):
+    print(a + b)
+    return a+b
+
+# Subtraction
+
+def subtract(minuend,subtrahend):
+    print(minuend-subtrahend)
+    return minuend-subtrahend
+
+# Multiplication
+
+def multiply(a,b):
+    print(a*b)
+    return a*b
+
+# Division
+
+def divide(dividend,divisor):
+    print(dividend/divisor)
+    return dividend/divisor
+
+# Square
+
+def square(base):
+    print(base ** 2)
+    return base ** 2
+
+# Exponent
+
+def exp(base, exponent):
+    print(base**exponent)
+    return base**exponent
+
+# Square Root
+
+def square_root(n):
+    print(n ** (1 / 2))
+    return n ** (1 / 2)
+
+# Inverse / Reciprocal
+
+def inv(n):
+    print(1/n)
+    return 1/n
+
+#### Trig
+
+def sine(x):
+    print(math.sin(x))
+    return math.sin(x)
+
+def cosine(x):
+    print(math.cos(x))
+    return math.cos(x)
+
+def tangent(x):
+    print(math.tan(x))
+    return math.tan(x)
+
+def invSin(x):
+    print(math.asin(x))
+    return math.asin(x)
+
+def invCos(x):
+    print(math.acos(x))
+    return math.acos(x)
+
+def invTan(x):
+    print(math.atan(x))
+    return math.atan(x)
+
+# Switch Sign
+
+def switch_sign(n):
+    print(-1*n)
+    return -1 * n
+
+# Factorial
+
+def factorial(n):
+    print(math.factorial(n))
+    return math.factorial(n)
+
+# Log Base 10
+
+def log_base_10(n):
+    print(math.log10(n))
+    return math.log10(n)
+
+# Inverse log 10^x
+
+def inv_log_10(n):
+    print(10**n)
+    return 10**n
+
+# Log Base e
+
+def log_base_e(n):
+    print(math.log(n))
+    return math.log(n)
+
+# Inverse ln
+
+def inv_log_e(n):
+    print(math.exp(n))
+    return math.exp(n)
+
+# Log Base Custom
+
+def log_base_y(n, base):
+    print(math.log(n, base))
+    return math.log(n, base)
+
+def choose_data_type():
+    data_choice = int(input('Enter data type: 1. Decimal 2. Hexadecimal 3. Binary 4. Octal '))
+    if data_choice == 1:
+        print(main_result)
+        return main_result
+    elif data_choice == 2:
+        print(hex(int(main_result)))
+        return hex(int(main_result))
+    elif data_choice == 3:
+        print(bin(int(main_result)).replace("0b", ""))
+        return bin(int(main_result)).replace("0b", "")
+    elif data_choice == 4:
+        print(oct(int(main_result)))
+        return oct(int(main_result))
+
+
+#### Small Loop ####
+
+def choose_small_op():
+    print('1 Addition           2 Subtraction       3 Multiplication        4 Division')
+    print('5 Square             6 Power             7 Square Root           8 Inverse')
+    print('9 M+                 10 MRC              11 MC                   12 Exit')
+    print(' ')
+    op = int(input('Choose an operation: '))
+
+    memStore = None
+
+    if op == 1:
+        print('Addition')
+        print('Enter the second addend: ')
+        addend2 = enter_num()
+        result = add2(addend2)
+        return result
+
+    elif op == 2:
+        print('Subtraction')
+        print('Enter the subtrahend: ')
+        subtrahend = enter_num()
+        result = subtract2(subtrahend)
+        return result
+
+    elif op == 3:
+        print('Multiplication')
+        print('Enter the second factor: ')
+        b = enter_num()
+        result = multiply2(b)
+        return result
+
+    elif op == 4:
+        print('Division')
+        print('Enter the divisor: ')
+        divisor = enter_num()
+        result = divide2(divisor)
+        return result
+
+    elif op == 5:
+        print('Square')
+        result = square2(main_result)
+        return result
+
+    elif op == 6:
+        print('Power')
+        print('Enter the exponent')
+        exponent = enter_num()
+        result = exp2(exponent)
+        return result
+
+    elif op == 7:
+        print('Square Root')
+        result = square_root2(main_result)
+        return result
+
+    elif op == 8:
+        print('Inverse')
+        #num = enter_num()
+        result = inv2(main_result)
+        return result
+
+    elif op == 9:
+        memStore = main_result
+        print(memStore)
+        return memStore
+
+    elif op == 10:
+        if type(memStore) != None:
+            print(memStore)
+        else:
+            print('Empty')
+
+    elif op == 11:
+        store_choice = input('Do you want to clear memory? Y/N: ').lower()
+        if store_choice == 'y':
+            memStore = 0
+        else:
+            pass
+        print(memStore)
+        return memStore
+
+    elif op == 12:
+        condi = False
+        print('Goodbye!')
+        
